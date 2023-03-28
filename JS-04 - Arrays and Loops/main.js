235dfd578c584b77b430a0d56fc296168cbd06f0
abelz <abelzapien65@gmail.com>
2023-03-27 18:40:13 -0600

    Se termina ejercicios faltantes, TERMINADO.

--- JS-04 - Arrays and Loops/main.js
+++ JS-04 - Arrays and Loops/main.js
@@ -25,34 +25,34 @@ for(num=[0];num < 5; num++){
 console.log("----------------Ejercicio #3---------------------")
 console.log("****************Parte #1*********************")
 let xValue = 5;
-while (xValue > 0) {
+while (xValue >= 0) {
     console.log(xValue);
     xValue -= .5;  
 }
 
 //Número impar = 2 · n − 1
-console.log("****************Parte #2XNo terminadoX*********************")
+console.log("****************Parte #2*********************")
 xValue = 1;
-while (xValue > 100) {
-    while (xValue == 2*xValue-1) {
-        console.log(xValue);
-    }
+while (xValue <= 50) {
+    console.log(2*xValue-1);
     xValue ++;  
 }
 
 console.log("****************Parte #3*********************")
 xValue = 10;
 num = 1;
-while (num < xValue+1) {
+while (num <= xValue) {
     console.log([num]);
     num ++;  
 }
 
-console.log("****************Parte #4XNo terminadoX*********************")
-xValue = 10;
-num = xValue;
+console.log("****************Parte #4*********************")
+xValue = 5;
+num = 0;
 let numer = 0;
-while (numer > num+1) {
-    console.log(xValue += num);
-    numer ++;  
+while (0 < xValue) {
+    console.log(numer += xValue);
+    num ++;  
+    xValue --;
+    
 }
